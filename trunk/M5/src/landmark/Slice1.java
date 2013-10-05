dcb5abb236b526164f4213ab917f34463cc00167
ross.lucier@gmail.com <ross.lucier@gmail.com@b17b0b61-ba01-fd7a-120a-343e1b6fe43f>
2013-10-05 18:08:29 +0000

    Added the title and players selection on the feature slice.

--- trunk/M5/src/landmark/Slice1.java
+++ trunk/M5/src/landmark/Slice1.java
@@ -6,6 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class Slice1 extends JFrame {
 
@@ -34,9 +40,33 @@ public class Slice1 extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 204, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 204, 0));
+		contentPane.add(panel, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("M.U.L.E.");
+		lblNewLabel.setFont(new Font("Garuda", Font.BOLD, 27));
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		panel.add(lblNewLabel);
+		
+		JPanel center = new JPanel();
+		contentPane.add(center, BorderLayout.CENTER);
+		
+		JLabel lblPlayers = new JLabel("Players");
+		lblPlayers.setFont(new Font("Garuda", Font.BOLD, 17));
+		center.add(lblPlayers);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setMaximumRowCount(4);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4"}));
+		comboBox.setForeground(Color.WHITE);
+		comboBox.setBackground(Color.BLACK);
+		center.add(comboBox);
 	}
 
 }
