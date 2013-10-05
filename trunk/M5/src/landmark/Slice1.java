ef500bc6ead7ea0ebac6a38a51ea4ea4535e1d1c
ross.lucier@gmail.com <ross.lucier@gmail.com@b17b0b61-ba01-fd7a-120a-343e1b6fe43f>
2013-10-05 17:27:06 +0000

    Added a feature slice file called Slice1.java.

---
+++ trunk/M5/src/landmark/Slice1.java
@@ -0,0 +1,42 @@
+package landmark;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Slice1 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Slice1 frame = new Slice1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Slice1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
