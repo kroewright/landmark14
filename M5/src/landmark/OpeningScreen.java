0ff8f50b66e0d5cf02c082d0812b1a999a09a873
kroewright <kroewright@gmail.com>
2013-10-07 23:00:59 +0000

    Added javadocs to the Opening Page and Selection Page files. Fixed indentation also.

--- M5/src/landmark/OpeningScreen.java
+++ M5/src/landmark/OpeningScreen.java
@@ -25,6 +25,11 @@ import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
 
+/**
+ * 
+ * @author Landmark - Team 14
+ *
+ */
 public class OpeningScreen extends JFrame {
 	private int isStandard = 0; // 1 = standard map and 2 = random map
 	
@@ -60,7 +65,6 @@ public class OpeningScreen extends JFrame {
 	 /**
 	  * Create the frame.
 	  */
-
 	 public OpeningScreen() {
 		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		 setBounds(100, 100, 953, 617);
@@ -71,7 +75,11 @@ public class OpeningScreen extends JFrame {
 		 setContentPane(contentPane);
 
 
-
+		 /*
+		  * Reads file containing picture for opening
+		  * and uses try catch to make sure the file 
+		  * is really there.
+		  */
 		 JPanel center = new JPanel();
 		 contentPane.add(center, BorderLayout.CENTER);
 		 BufferedImage myPicture = null;
@@ -85,6 +93,11 @@ public class OpeningScreen extends JFrame {
 		 }
 		 center.setLayout(null);
 
+		 
+		 /*
+		  * Action listener for button number 1.
+		  * Sets button location.
+		  */
 		 JButton btn2 = new JButton("1");
 		 btn2.setBackground(new Color(204, 255, 204));
 		 btn2.addActionListener(new ActionListener() {
@@ -95,6 +108,11 @@ public class OpeningScreen extends JFrame {
 		 btn2.setBounds(301, 371, 46, 23);
 		 center.add(btn2);
 
+		 
+		 /*
+		  * Action listener for button number 2.
+		  * Sets button location.
+		  */
 		 JButton btn3 = new JButton("2");
 		 btn3.addActionListener(new ActionListener() {
 			 public void actionPerformed(ActionEvent arg3) {
@@ -103,7 +121,11 @@ public class OpeningScreen extends JFrame {
 		 });
 		 btn3.setBounds(387, 371, 46, 23);
 		 center.add(btn3);
-
+		 
+		 /*
+		  * Action listener for button number 3.
+		  * Sets button location.
+		  */
 		 JButton btn1 = new JButton("3");
 		 btn1.addActionListener(new ActionListener() {
 			 public void actionPerformed(ActionEvent arg1) {
@@ -112,7 +134,11 @@ public class OpeningScreen extends JFrame {
 		 });
 		 btn1.setBounds(478, 371, 46, 23);
 		 center.add(btn1);
-
+		 
+		 /*
+		  * Action listener for button number 4.
+		  * Sets button location.
+		  */
 		 JButton btn4 = new JButton("4");
 		 btn4.addActionListener(new ActionListener() {
 			 public void actionPerformed(ActionEvent arg4) {
@@ -121,22 +147,35 @@ public class OpeningScreen extends JFrame {
 		 });
 		 btn4.setBounds(568, 371, 46, 23);
 		 center.add(btn4);
+		 
+		 /**
+		  * JLabel for picture.
+		  * Sets the bounds before adding the label.
+		  */
 		 JLabel picLabel = new JLabel(new ImageIcon(myPicture));
 		 picLabel.setBounds(27, 5, 869, 518);
 		 center.add(picLabel);
 
-
+		 /**
+		  * Set the background color for the lower part of the
+		  * opening screen. 
+		  */
 		 JPanel south = new JPanel();
 		 south.setBackground(new Color(255, 204, 0));
 		 contentPane.add(south, BorderLayout.SOUTH);
 		 south.setLayout(new GridLayout(0, 8, 0, 0));
 
+		 /**
+		  * Creates the label for Map Type.
+		  */
 		 JLabel lblNewLabel = new JLabel("Map Type:");
 		 lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		 lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 17));
 		 south.add(lblNewLabel);
 
-		 
+		 /**
+		  * Creates first radio button for Standard map type.
+		  */
 		 JRadioButton rdbtnNewRadioButton = new JRadioButton("Standard");
 		 rdbtnNewRadioButton.addActionListener(new ActionListener() {
 		 	public void actionPerformed(ActionEvent arg0) {
@@ -145,6 +184,9 @@ public class OpeningScreen extends JFrame {
 		 });
          rdbtnNewRadioButton.setHorizontalAlignment(SwingConstants.LEFT);
          
+         /**
+          * Creates second radio button for Random map type.
+          */
          JRadioButton rdbtnRandom = new JRadioButton("Random");
          rdbtnRandom.addActionListener(new ActionListener() {
          	public void actionPerformed(ActionEvent e) {
@@ -153,18 +195,31 @@ public class OpeningScreen extends JFrame {
          });
          rdbtnRandom.setHorizontalAlignment(SwingConstants.LEFT);
          
+         /**
+          * Adds the buttons to the button group.
+          */
          ButtonGroup bg1 = new ButtonGroup( );
          bg1.add(rdbtnNewRadioButton);
          bg1.add(rdbtnRandom);
          
+         /**
+          * Adds the buttons to the panel.
+          */
          south.add(rdbtnNewRadioButton);
          south.add(rdbtnRandom);
 
+         /**
+          * Creates JLabel for Difficulty.
+          * Adds it to the panel.
+          */
 		 JLabel lblDifficulty = new JLabel("Difficulty:");
 		 lblDifficulty.setFont(new Font("Garuda", Font.BOLD, 17));
 		 lblDifficulty.setHorizontalAlignment(SwingConstants.RIGHT);
 		 south.add(lblDifficulty);
 
+		 /**
+		  * Creates new radio button for Beginner level.	
+		  */
 		 JRadioButton rdbtnBeginner = new JRadioButton("Beginner");
 		 rdbtnBeginner.addActionListener(new ActionListener() {
 		 	public void actionPerformed(ActionEvent e) {
@@ -172,9 +227,15 @@ public class OpeningScreen extends JFrame {
 		 	}
 		 });
 		 
+		 /**
+		  * Set background and aligns radio button.
+		  */
 		 rdbtnBeginner.setHorizontalAlignment(SwingConstants.LEFT);
 		 rdbtnBeginner.setBackground(new Color(255, 204, 0));
-
+		 
+		 /**
+		  * Creates new radio button for Advanced level.	
+		  */
 		 JRadioButton rdbtnAdvanced = new JRadioButton("Advanced");
 		 rdbtnAdvanced.addActionListener(new ActionListener() {
 		 	public void actionPerformed(ActionEvent e) {
@@ -182,9 +243,15 @@ public class OpeningScreen extends JFrame {
 		 	}
 		 });
 		 
+		 /**
+		  * Set background and aligns radio button.
+		  */
 		 rdbtnAdvanced.setHorizontalAlignment(SwingConstants.LEFT);
 		 rdbtnAdvanced.setBackground(new Color(255, 204, 0));
 
+		 /**
+		  * Creates new radio button for Tournament level.	
+		  */
 		 JRadioButton rdbtnTournament = new JRadioButton("Tournament");
 		 rdbtnTournament.addActionListener(new ActionListener() {
 		 	public void actionPerformed(ActionEvent e) {
@@ -192,18 +259,31 @@ public class OpeningScreen extends JFrame {
 		 	}
 		 });
 		 
+		 /**
+		  * Set background and aligns radio button.
+		  */
 		 rdbtnTournament.setHorizontalAlignment(SwingConstants.LEFT);
 		 rdbtnTournament.setBackground(new Color(255, 204, 0));
 
+		 /**
+          * Adds the buttons to the button group.
+          */
 		 ButtonGroup bg2 = new ButtonGroup( );
 		 bg2.add(rdbtnBeginner);
 		 bg2.add(rdbtnAdvanced);
 		 bg2.add(rdbtnTournament);
 
+		 /**
+          * Adds the buttons to the panel.
+          */
 		 south.add(rdbtnBeginner);
 		 south.add(rdbtnAdvanced);
 		 south.add(rdbtnTournament);
 
+		 /**
+		  * Creates Next button after players, map, and difficulty 
+		  * have been selected.
+		  */
 		 JButton btnStart = new JButton("NEXT");
 		 btnStart.addActionListener(new ActionListener() {
 			 public void actionPerformed(ActionEvent e) {
@@ -211,19 +291,18 @@ public class OpeningScreen extends JFrame {
 					 contentPane.setVisible(false);
 					 
 					 
-					 contentPane2 = new JPanel();
-					 contentPane2.setBackground(new Color(255, 204, 0));
-					 contentPane2.setBorder(new EmptyBorder(5, 5, 5, 5));
-					 contentPane2.setLayout(new BorderLayout(0, 0));
+					 contentPane2 = new JPanel(); //new JPanel
+					 contentPane2.setBackground(new Color(255, 204, 0)); //sets background
+					 contentPane2.setBorder(new EmptyBorder(5, 5, 5, 5)); //sets border
+					 contentPane2.setLayout(new BorderLayout(0, 0)); //sets layout
 					 setContentPane(contentPane2);
 					 
-					 Selpage selectionPage = new Selpage();
-					 contentPane2.add(selectionPage.generate(numOfPlayers));
-				
-				 }
-
+					 Selpage selectionPage = new Selpage(); //new selection page
+					 contentPane2.add(selectionPage.generate(numOfPlayers)); //allows number of players to be seen on
+				 }																	//the opening and selection screens
 			 }
 		 });
+		 //Add Next button to panel
 		 south.add(btnStart);
 
 	 }
