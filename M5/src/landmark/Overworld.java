56731d79960e3c5bdefd4f32cbfbb5c12d37500a
ross.lucier <ross.lucier@gmail.com>
2013-10-13 20:47:18 +0000

    Edited the Overworld class to create a 5x9 grid of buttons.

--- M5/src/landmark/Overworld.java
+++ M5/src/landmark/Overworld.java
@@ -5,37 +5,56 @@ import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
 public class Overworld extends JPanel {
 	
-	//private Tiles[] tiles;
+	//private Tiles[][] tiles;
 	private BufferedImage picture;
 	private JPanel panel;
+	private JButton[][] buttons;
+	private ImageIcon riverImage;
+	private ImageIcon plainImage;
+	private ImageIcon mountainImage;
 	
 	/**
 	* Create the panel.
 	*/
 	public Overworld() {
-		panel = new JPanel();
-		try 
-	    {
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-			InputStream input = classLoader.getResourceAsStream("Show_Players_Fake.jpg");
-			picture = ImageIO.read(input);
-	    } 
-	    catch(IOException e) 
-	    {
-	      e.printStackTrace();
-	    }
+		super(new GridLayout(5, 9));
+		buttons = new JButton[5][9];
+		setPreferredSize(new Dimension(800, 800));
 		
-		panel.setPreferredSize(new Dimension(800, 800));
+		/*
+		riverImage = new ImageIcon(XXX);
+		plainImage = new ImageIcon(XXX);
+		mountainImage = new ImageIcon(XXX);
+		
+		Plain p = new Plain();
+		River r = new River();
+		Mountain m1 = new Mountain(1);
+		Mountain m2 = new Mountain(2);
+		Mountain m3 = new Mountain(3);
+		*/
+		
+		
+		for(int i = 0; i < 5; ++i) {
+			for(int j = 0; j < 9; ++j) {
+				addTile(i, j);
+			}
+		}
+		
+		//Still need to go through buttons and add appro. tile type
 		
 		/**
 		 * JLabel for picture.
@@ -43,23 +62,52 @@ public class Overworld extends JPanel {
 		 */
 		JLabel picLabel = new JLabel(new ImageIcon(picture));   //ib
 		picLabel.setBounds(0, 0, 800, 800);						//ib
-		panel.add(picLabel);
-
-		
-		
+		panel.add(picLabel);	
 	}
 	
-	/*
-	public addTile(Tile mapPiece) {
-		
+	public JButton addTile(int i, int j) {
+		JButton button = new JButton("P");
+		buttons[i][j] = button;
+		//tiles[0][0] = new Plain();
+		button.addActionListener(new pressedButton(button, i ,j));
+		add(button);
+		return button;
 	}
-	*/
 	
-	protected void paintComponent(Graphics g) 
-	  {
-	    super.paintComponent(g);
-	    g.drawImage(picture, 0, 0, getWidth(), getHeight(), this);
-	  }
+	private class pressedButton implements ActionListener
+	{
+		private JButton button;
+		private int i;
+		private int j;
+		
+		/**
+		 * A constructor that takes in the pressed button and its
+		 * coordinates on the GridLayout so it can be picked from
+		 * the array.
+		 * 
+		 * @param button
+		 * @param i
+		 * @param j
+		 */
+		public pressedButton(JButton button, int i, int j)
+		{
+			this.button = button;
+			this.i = i;
+			this.j = j;
+		}
+		
+		/**
+		 * This method runs when the button is pushed. It checks 
+		 * to see if the pushed button is a valid move, moves 
+		 * the player to the spot if the move is valid, and then gets
+		 * the new valid moves and changes their color to yellow.
+		 * 
+		 * @param e
+		 */
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
 	
 	   public JComponent getMainComponent() {
            return panel;
