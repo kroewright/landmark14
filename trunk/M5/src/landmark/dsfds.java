b87bf5a41852ee1098782646bad2552bc192ed9b
ross.lucier@gmail.com <ross.lucier@gmail.com@b17b0b61-ba01-fd7a-120a-343e1b6fe43f>
2013-10-13 20:47:18 +0000

    Edited the Overworld class to create a 5x9 grid of buttons.

---
+++ trunk/M5/src/landmark/dsfds.java
@@ -0,0 +1,21 @@
+package landmark;
+
+import javax.swing.JFrame;
+
+public class dsfds {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		Overworld m = new Overworld();
+		f.add(m);
+		 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setBounds(100, 100, 953, 500);
+		 f.setVisible(true);
+		
+
+	}
+
+}
