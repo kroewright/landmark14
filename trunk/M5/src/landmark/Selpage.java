4a86a0fce2f4e7e907636090d895aee3c37ba145
ib.gatech@gmail.com <ib.gatech@gmail.com@b17b0b61-ba01-fd7a-120a-343e1b6fe43f>
2013-10-18 04:37:57 +0000

    I added javadocs to GameDriver and Selpage.

--- trunk/M5/src/landmark/Selpage.java
+++ trunk/M5/src/landmark/Selpage.java
@@ -1,10 +1,5 @@
 package landmark;
 
-//import BadColorException;
-//import BadNameException;
-//import BadRaceException;
-//import Player;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionListener;
@@ -45,21 +40,29 @@ public class Selpage extends JPanel {
 	private JTextField txtPlayer3;
 	private JTextField txtPlayer4;
 
+	@SuppressWarnings("rawtypes")
 	private JComboBox colorBox1;
+	@SuppressWarnings("rawtypes")
 	private JComboBox colorBox2;
+	@SuppressWarnings("rawtypes")
 	private JComboBox colorBox3;
+	@SuppressWarnings("rawtypes")
 	private JComboBox colorBox4;
 
+	@SuppressWarnings("rawtypes")
 	private JComboBox raceBox1;
+	@SuppressWarnings("rawtypes")
 	private JComboBox raceBox2;
+	@SuppressWarnings("rawtypes")
 	private JComboBox raceBox3;
+	@SuppressWarnings("rawtypes")
 	private JComboBox raceBox4;
 
 	private JButton startButton;
-	private int playerCount;
+	//private int playerCount;
 
 	/**
-	 * Create the application.
+	 * The Constructor creates the panel for the players selection screen .
 	 */
 	public Selpage() {
 		panel = new JPanel();
@@ -74,6 +77,13 @@ public class Selpage extends JPanel {
 		panel.add(startButton);
 	}
 
+	/**
+	 * The setPage method sets the right configuration
+	 * of the selection screen based on the number of players picked.
+	 * 
+	 * @param numOfPlayers
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void setSelpage(int numOfPlayers){
 		BufferedImage myPicture = null;
 		//Try-catch for image for selection screen
@@ -86,11 +96,7 @@ public class Selpage extends JPanel {
 		}
 		panel.setLayout(null);
 
-		//Can't play if there are more than 4 players
-		// if (numOfPlayers > 4)
-		// return null;
-
-		playerCount = 0;
+		//playerCount = 0;
 
 		/**
 		 * If 0<x<=4 players have been selected,
@@ -109,17 +115,19 @@ public class Selpage extends JPanel {
 
 			//First dropdown menu containing race	
 			raceBox1 = new JComboBox();
-			raceBox1.setModel(new DefaultComboBoxModel(new String[] {"Business", "Scientist", "Engineer", "CS"}));
+			raceBox1.setModel(new DefaultComboBoxModel(
+					new String[] {"Business", "Scientist", "Engineer", "CS"}));
 			raceBox1.setToolTipText("");
 			raceBox1.setBounds(183, 237, 105, 20);
 			panel.add(raceBox1);
 
 			//Second dropdown menu containing player color
 			colorBox1 = new JComboBox();
-			colorBox1.setModel(new DefaultComboBoxModel(new String[] {"Gold", "Navy", "White", "Black"}));
+			colorBox1.setModel(new DefaultComboBoxModel(
+					new String[] {"Gold", "Navy", "White", "Black"}));
 			colorBox1.setBounds(183, 183, 105, 20);
 			panel.add(colorBox1);
-			playerCount += 1;
+			//playerCount += 1;
 		}
 		/**
 		 * If 0<x<=4 players have been selected,
@@ -138,17 +146,19 @@ public class Selpage extends JPanel {
 
 			//First dropdown menu containing race	
 			colorBox2 = new JComboBox();
-			colorBox2.setModel(new DefaultComboBoxModel(new String[] {"Gold", "Navy", "White", "Black"})); 
+			colorBox2.setModel(new DefaultComboBoxModel(
+					new String[] {"Gold", "Navy", "White", "Black"})); 
 			colorBox2.setBounds(302, 185, 105, 20);
 			panel.add(colorBox2);
 
 			//Second dropdown menu containing race		
 			raceBox2 = new JComboBox();
-			raceBox2.setModel(new DefaultComboBoxModel(new String[] {"Business", "Scientist", "Engineer", "CS"}));
+			raceBox2.setModel(new DefaultComboBoxModel(
+					new String[] {"Business", "Scientist", "Engineer", "CS"}));
 			raceBox2.setToolTipText("");
 			raceBox2.setBounds(302, 237, 105, 20);  
 			panel.add(raceBox2);
-			playerCount += 1;
+			//playerCount += 1;
 		}
 
 		/**
@@ -168,17 +178,19 @@ public class Selpage extends JPanel {
 
 			//First dropdown menu containing race	
 			colorBox3 = new JComboBox();
-			colorBox3.setModel(new DefaultComboBoxModel(new String[] {"Gold", "Navy", "White", "Black"}));
+			colorBox3.setModel(new DefaultComboBoxModel(
+					new String[] {"Gold", "Navy", "White", "Black"}));
 			colorBox3.setBounds(417, 185, 105, 20);
 			panel.add(colorBox3);
 
 			//Second dropdown menu containing race		
 			raceBox3 = new JComboBox();
-			raceBox3.setModel(new DefaultComboBoxModel(new String[] {"Business", "Scientist", "Engineer", "CS"}));
+			raceBox3.setModel(new DefaultComboBoxModel(
+					new String[] {"Business", "Scientist", "Engineer", "CS"}));
 			raceBox3.setToolTipText("");
 			raceBox3.setBounds(417, 237, 105, 20);
 			panel.add(raceBox3);
-			playerCount += 1;
+			//playerCount += 1;
 		}
 
 		/**
@@ -198,30 +210,43 @@ public class Selpage extends JPanel {
 
 			//First dropdown menu containing race		
 			colorBox4 = new JComboBox();
-			colorBox4.setModel(new DefaultComboBoxModel(new String[] {"Gold", "Navy", "White", "Black"}));
+			colorBox4.setModel(new DefaultComboBoxModel(
+					new String[] {"Gold", "Navy", "White", "Black"}));
 			colorBox4.setBounds(527, 185, 105, 20);
 			panel.add(colorBox4);
 
 			//Second dropdown menu containing race		
 			raceBox4 = new JComboBox();
-			raceBox4.setModel(new DefaultComboBoxModel(new String[] {"Business", "Scientist", "Engineer", "CS"}));
+			raceBox4.setModel(new DefaultComboBoxModel(
+					new String[] {"Business", "Scientist", "Engineer", "CS"}));
 			raceBox4.setToolTipText("");
 			raceBox4.setBounds(527, 237, 105, 20);
 			panel.add(raceBox4);
-			playerCount += 1;
+			//playerCount += 1;
 		}
 
 		//JLabel for picture, creates bounds
 		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
 		picLabel.setBounds(0, 0, 792, 576);
 		panel.add(picLabel);
-		// return panel;
 	}	
 
+	/**
+	 * The addStartButtonActionListener method adds the action listener 
+	 * for the Start button.
+	 * 
+	 * @param listener
+	 */
 	public void addStartButtonActionListener(ActionListener listener) {
 		startButton.addActionListener(listener);//{
 	}
 	
+	/**
+	 * The createPlayers method configures players.
+	 * 
+	 * @param numOfPlayers
+	 * @return Returns array of players
+	 */
 	public Player[] createPlayers(int numOfPlayers) {  
 		players = new Player[numOfPlayers];
 		int i = players.length - numOfPlayers;
@@ -331,84 +356,21 @@ public class Selpage extends JPanel {
 		return players;
 	}
 
-
-
-	//	 startButton.addActionListener(new ActionListener(){
-	//		 public void actionPerformed(ActionEvent e){
-	/*public void assign() {
-		if(!txtPlayer1.getText().isEmpty() && playerCount == 1) {
-			player1 = new Player(txtPlayer1.getText(), colorBox1.getSelectedIndex(), difficulty,
-					raceBox1.getSelectedIndex());
-		}
-		else if(!txtPlayer1.getText().isEmpty() && !txtPlayer2.getText().isEmpty() && playerCount == 2) {
-			player1 = new Player(txtPlayer1.getText(), colorBox1.getSelectedIndex(), difficulty,
-					raceBox1.getSelectedIndex());
-
-			player2 = new Player(txtPlayer2.getText(), colorBox2.getSelectedIndex(), difficulty,
-					raceBox2.getSelectedIndex());
-		}
-		else if(!txtPlayer1.getText().isEmpty() && !txtPlayer2.getText().isEmpty()
-				&& !txtPlayer3.getText().isEmpty() && playerCount == 3) {
-			player1 = new Player(txtPlayer1.getText(), colorBox1.getSelectedIndex(), difficulty,
-					raceBox1.getSelectedIndex());
-
-			player2 = new Player(txtPlayer2.getText(), colorBox2.getSelectedIndex(), difficulty,
-					raceBox2.getSelectedIndex());
-
-			player3 = new Player(txtPlayer3.getText(), colorBox3.getSelectedIndex(), difficulty,
-					raceBox3.getSelectedIndex());
-		}
-		else if(!txtPlayer1.getText().isEmpty() && !txtPlayer2.getText().isEmpty()
-				&& !txtPlayer3.getText().isEmpty() && !txtPlayer4.getText().isEmpty() && playerCount == 4) {
-			player1 = new Player(txtPlayer1.getText(), colorBox1.getSelectedIndex(), difficulty,
-					raceBox1.getSelectedIndex());
-
-			player2 = new Player(txtPlayer2.getText(), colorBox2.getSelectedIndex(), difficulty,
-					raceBox2.getSelectedIndex());
-
-			player3 = new Player(txtPlayer3.getText(), colorBox3.getSelectedIndex(), difficulty,
-					raceBox3.getSelectedIndex());
-
-			player4 = new Player(txtPlayer4.getText(), colorBox4.getSelectedIndex(), difficulty,
-					raceBox4.getSelectedIndex());
-		}
-		//If there is no player 1, it will not continue
-		if(player1.getName() != null) {
-			panel.setVisible(false);
-		}
-	}
-*/
+	/**
+	 * The getMainComponent method returns the panel to show 
+	 * on the on the frame.
+	 * 
+	 * @return
+	 */
 	public JComponent getMainComponent() {
 		return panel;
 	}
 	
+	/**
+	 * The setDifficulty method sets the level picked by the player(s).
+	 * @param level
+	 */
 	public void setDifficulty(int level) {
 		difficulty = level;
 	}
-
-	/*public Player[] getPlayers() {
-		Player[] players = new Player[1];
-		if(playerCount == 1) {
-			players[0] = player1;
-		}
-		else if(playerCount ==2) {
-			players = new Player[2];
-			players[0] = player1;
-			players[1] = player2;
-		}
-		else if(playerCount ==3) {
-			players = new Player[3];
-			players[0] = player1;
-			players[1] = player2;
-			players[2] = player3;
-		}
-		else if(playerCount == 4) {
-			players = new Player[4];
-			players[0] = player1;
-			players[1] = player2;
-			players[2] = player3;
-			players[3] = player4;
-		}
-		return players;
-	}*/
 }
