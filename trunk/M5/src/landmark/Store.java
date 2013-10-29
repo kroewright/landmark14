85e96d790f733708b533cf3a2a9c0545bf1c522f
kroewright@gmail.com <kroewright@gmail.com@b17b0b61-ba01-fd7a-120a-343e1b6fe43f>
2013-10-29 00:12:18 +0000

    New Store class to work with Energy, Food, and Ores.

---
+++ trunk/M5/src/landmark/Store.java
@@ -0,0 +1,21 @@
+package landmark;
+
+import java.awt.*;
+
+/**
+ * 
+ * @author landmark - team 14
+ * 
+ */
+
+public class Store {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
