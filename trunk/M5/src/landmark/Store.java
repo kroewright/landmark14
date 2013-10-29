defc75d8b96bbadb17ca49adc912bbaf32d6efeb
kroewright@gmail.com <kroewright@gmail.com@b17b0b61-ba01-fd7a-120a-343e1b6fe43f>
2013-10-29 01:17:51 +0000

    Button listeners are being added to the store panel in this commit.

--- trunk/M5/src/landmark/Store.java
+++ trunk/M5/src/landmark/Store.java
@@ -1,6 +1,10 @@
 package landmark;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
 
 /**
  * 
@@ -9,13 +13,42 @@ import java.awt.*;
  */
 
 public class Store {
+	
+	public Player player;
+	public TownPanel panel;
+	
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int invFood;
+	private int invEnergy;
+	private int invOre;
+	private int invMules;
+	
+	private final int foodCost = 30;
+	private final int energyCost = 25;
+	private final int oreCost = 50;
+	private final int muleCost = 100;
+	
+	//private Tiles[] landForSale;
+	
+	
+	public Store(Player player, TownPanel panel) {
+		panel = panel.getTP();
+		player = panel.getPlayer();	
 	}
+	
+	//if player clicks on ore
+	//ACTION LISTENER
+	addOreBtnActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
 
+			JOptionPane.showOptionDialogue;
+
+			//Ends the player's turn and activate the another player
+			if(turn == (players.length - 1)) {
+				turn = 0;
+				//players = map.orderPlayersByScore(players);
+			}
+			else {
+				turn += 1;
+			}
 }
