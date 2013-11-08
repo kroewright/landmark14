1ca074ed2652f18904986cf38dfeaa54200bae74
kroewright <kroewright@gmail.com>
2013-11-08 19:46:46 +0000

    Updating javadocs.

--- M5/src/landmark/BadNameException.java
+++ M5/src/landmark/BadNameException.java
@@ -1,5 +1,12 @@
 package landmark;
 
+/**
+ * 
+ * @author landmark - team 14
+ * Makes sure that a player enters a name in the text field.
+ *
+ */
+
 @SuppressWarnings("serial")
 public class BadNameException extends Exception {
 
