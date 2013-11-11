abe13aeacc902d72ee3dcbec57432bf9a80cb137
kroewright <kroewright@gmail.com>
2013-11-11 19:42:04 +0000

    Updated javadocs

--- M5/src/landmark/PresenterStoreTownPanel.java
+++ M5/src/landmark/PresenterStoreTownPanel.java
@@ -4,6 +4,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.Serializable;
 
+/**
+ * 
+ * @author landmark - team 14
+ * 
+ * Class that adds action listeners to the town panel for the 
+ * purchase of store items.
+ *
+ */
 public class PresenterStoreTownPanel implements Serializable {
 	public static Store store;
 	private TownPanel town;
@@ -15,6 +23,13 @@ public class PresenterStoreTownPanel implements Serializable {
 		
 	}
 	
+	/**
+	 * Constructor that contains the town panel and the store in
+	 * which the action listeners are added to.
+	 * 
+	 * @param town
+	 * @param store
+	 */
 	public static void addListeners(TownPanel town, final Store store){
 	
 		town.setOreActionListener(new ActionListener() {
